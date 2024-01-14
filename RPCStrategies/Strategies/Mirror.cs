34c75c05a105c57bbc9cc4afeb95f1a2d7c6dbce
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-01-13 22:36:41 -0500

    Mirror strat

---
+++ RPCStrategies/Strategies/Mirror.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCStrategies.Strategies
+{
+    internal class Mirror : AbstractStrategy
+    {
+        Hand OpponentsLastPick;
+
+        public new void Pick()
+        {
+            Hand = OpponentsLastPick switch
+            {
+                Hand.ROCK => Hand.ROCK,
+                Hand.PAPER => Hand.PAPER,
+                Hand.SCISSORS => Hand.SCISSORS,
+                _ => Hand.NOT_PICKED,
+            };
+        }
+    }
+}
