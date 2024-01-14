a172052096295d8471beb59d134ebbb068836cf8
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-01-13 22:37:32 -0500

    Counter

---
+++ RPCStrategies/Strategies/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCStrategies.Strategies
+{
+    internal class Counter : AbstractStrategy
+    {
+        Hand OpponentsLastPick;
+
+        public new void Pick()
+        {
+            Hand = OpponentsLastPick switch
+            {
+                Hand.ROCK => Hand.PAPER,
+                Hand.PAPER => Hand.SCISSORS,
+                Hand.SCISSORS => Hand.ROCK,
+                _ => Hand.NOT_PICKED,
+            };
+        }
+    }
+}
