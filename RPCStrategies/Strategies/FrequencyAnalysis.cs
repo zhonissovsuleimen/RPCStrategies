49212e899822c5e356c0e99fc1598f4926a1b114
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-01-14 13:47:00 -0500

    frequency analysis strat

---
+++ RPCStrategies/Strategies/FrequencyAnalysis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCStrategies.Strategies
+{
+    internal class FrequencyAnalysis : AbstractStrategy
+    {
+        List<Hand> opponentHandHistory = new List<Hand>();
+
+        public new void Pick()
+        {
+            if (opponentHandHistory.Count == 0)
+            {
+                Hand = GetRandomHand();
+                return;
+            }
+
+            var random = new System.Random();
+            var randomIndex = random.Next(0, opponentHandHistory.Count);
+            Hand = GetCounter(opponentHandHistory[randomIndex]);
+        }
+        public new void Play(Hand opponentHand)
+        {
+            opponentHandHistory.Add(opponentHand);
+        }
+    }
+}
