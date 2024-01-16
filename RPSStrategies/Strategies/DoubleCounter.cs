12d0ee7f0a5dcff8f89ac5b4843cc5917f03147d
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-01-16 17:45:12 -0500

    double counter strat

---
+++ RPSStrategies/Strategies/DoubleCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSStrategies.Strategies
+{
+    internal class DoubleCounter : AbstractStrategy
+    {
+        Hand OpponentsLastPick = Hand.NOT_PICKED;
+
+        public override void Pick()
+        {
+            if (OpponentsLastPick == Hand.NOT_PICKED)
+            {
+                Hand = GetRandomHand();
+                return;
+            }
+
+            Hand FirstCounter = GetCounter(OpponentsLastPick);
+            Hand OpponentsCounter = GetCounter(FirstCounter);
+            Hand = GetCounter(OpponentsCounter);
+        }
+
+        public override void Play(Hand opponentHand)
+        {
+            OpponentsLastPick = opponentHand;
+        }
+
+        public override void Reset()
+        {
+            OpponentsLastPick = Hand.NOT_PICKED;
+        }
+    }
+}
