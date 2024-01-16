ae21221d33df2009587d27584619536c57234416
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-01-16 17:51:43 -0500

    Cyclic strat

---
+++ RPSStrategies/Strategies/Cyclic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSStrategies.Strategies
+{
+    internal class Cyclic : AbstractStrategy
+    {
+        Hand[] Sequence = new Hand[3];
+        uint counter = 0;
+
+        public Cyclic()
+        {
+            GenerateSequence();
+        }
+
+        public override void Pick()
+        {
+            Hand = Sequence[counter];
+            counter = (counter + 1) % 3;
+        }
+
+        public override void Play(Hand opponentHand) {}
+
+        public override void Reset()
+        {
+            GenerateSequence();
+        }
+
+        void GenerateSequence()
+        {
+            Sequence[0] = GetRandomHand();
+            Sequence[1] = GetRandomHand();
+            Sequence[2] = GetRandomHand();
+        }
+    }
+}
