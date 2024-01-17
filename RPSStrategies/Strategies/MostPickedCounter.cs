a444e4a6617d2b1e9aadc37872fa5e91c56665f8
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-01-16 21:35:27 -0500

    most picked counter

---
+++ RPSStrategies/Strategies/MostPickedCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSStrategies.Strategies
+{
+    internal class MostPickedCounter : AbstractStrategy
+    {
+        Dictionary<Hand, uint> count = new Dictionary<Hand, uint>();
+
+        public MostPickedCounter()
+        {
+            count.Add(Hand.ROCK, 0);
+            count.Add(Hand.PAPER, 0);
+            count.Add(Hand.SCISSORS, 0);
+        }
+
+        public override void Pick()
+        {
+            if (!count.Values.Any(v => v != 0))
+            {
+                Hand = GetRandomHand();
+                return;
+            }
+
+            var mostPickedHands = count.Where(kvp => kvp.Value == count.Values.Max()).Select(kvp => kvp.Key);
+
+            var random = new System.Random();
+            Hand mostPicked = mostPickedHands.ElementAt(random.Next(0, mostPickedHands.Count()));
+            Hand = GetCounter(mostPicked);
+        }
+        public override void Play(Hand opponentHand)
+        {
+            count[opponentHand] += 1;
+        }
+
+        public override void Reset()
+        {
+            count[Hand.ROCK] = 0;
+            count[Hand.PAPER] = 0;
+            count[Hand.SCISSORS] = 0;
+        }
+    }
+}
