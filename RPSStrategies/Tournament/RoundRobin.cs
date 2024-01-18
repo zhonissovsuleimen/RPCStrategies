c43a56725f33a19573d0d2a84144453fd24fa858
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-01-18 01:20:00 -0500

    little imporvements to round robin

--- RPSStrategies/Tournament/RoundRobin.cs
+++ RPSStrategies/Tournament/RoundRobin.cs
@@ -1,50 +1,51 @@
 ﻿using System.Text;
+using System.Linq;
 using RPSStrategies.Strategies;
+
 
 namespace RPSStrategies.Tournament
 {
     internal class RoundRobin
     {
         public List<AbstractStrategy> Players { get; init; }
-        //temporary scoring system
-        public List<int> Scores { get; init; }
+        Dictionary<AbstractStrategy, int> Scores = new();
 
-        public int WinReward { get; init; } = 2;
+        public int WinReward { get; init; } = 3;
         public int TieReward { get; init; } = 1;
         public int LossReward { get; init; } = 0;
 
-        public int SeriesRounds { get; init; } = 100;
-        public bool OvertimeEnabled { get; init; } = false;
+        public int SeriesRounds { get; set; } = 100;
+        public bool SeriesOvertimeEnabled { get; set; } = false;
 
         public RoundRobin(params AbstractStrategy[] players)
         {
             Players = new(players);
-            Scores = new List<int>(new int[players.Length]);
+            ResetScores();
         }
 
         public void Run()
         {
-            //temporary scoring system
+            ResetScores();
             for (int i = 0; i < Players.Count; i++)
             {
                 for (int j = i + 1; j < Players.Count; j++)
                 {
-                    Series series = new(Players[i], Players[j], SeriesRounds);
+                    Series series = new(Players[i], Players[j], SeriesRounds, SeriesOvertimeEnabled);
                     series.Play();
 
                     switch (series.Outcome)
                     {
                         case Outcome.PLAYER1_WIN:
-                            Scores[i] += WinReward;
-                            Scores[j] += LossReward;
+                            Scores[Players[i]] += WinReward;
+                            Scores[Players[j]] += LossReward;
                             break;
                         case Outcome.PLAYER2_WIN:
-                            Scores[i] += LossReward;
-                            Scores[j] += WinReward;
+                            Scores[Players[i]] += LossReward;
+                            Scores[Players[j]] += WinReward;
                             break;
                         case Outcome.TIE:
-                            Scores[i] += TieReward;
-                            Scores[j] += TieReward;
+                            Scores[Players[i]] += TieReward;
+                            Scores[Players[j]] += TieReward;
                             break;
                     }
                 }
@@ -54,11 +55,21 @@ namespace RPSStrategies.Tournament
         public string GetScores()
         {
             StringBuilder sb = new();
-            for (int i = 0; i < Players.Count; i++)
+            var scoresList = Scores.OrderByDescending(x => x.Value);
+            foreach(var score in scoresList)
             {
-                sb.Append($"{Players[i].GetType().Name}: {Scores[i]}\n");
+                sb.AppendLine($"{score.Key.GetType().Name}: {score.Value}");
             }
             return sb.ToString();
+        }
+
+        void ResetScores()
+        {
+            Scores.Clear();
+            for (int i = 0; i < Players.Count; i++)
+            {
+                Scores.Add(Players[i], 0);
+            }
         }
     }
 }
